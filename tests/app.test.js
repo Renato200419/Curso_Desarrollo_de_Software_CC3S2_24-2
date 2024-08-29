ddace12b15c6cd1b8fdf9ffbe62f492e0ed877db
Renato200419 <renato.olivera.c@uni.pe>
2024-08-28 20:57:48 -0500

    Agregado supertest y actualizados los tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,14 @@
 const request = require('supertest');
-const app = require('../src/app');
+const { app, server } = require('../src/app');  
 
 describe('GET /', () => {
+    afterAll(() => {
+        server.close();  // Cierra el servidor después de completar las pruebas
+    });
+
     it('should return Hello, World!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toBe('Hello, World!');
     });
 });
-
