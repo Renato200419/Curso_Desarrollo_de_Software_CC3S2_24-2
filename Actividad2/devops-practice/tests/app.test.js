3e406f10686628e1ca31aa693a55109475ae7e9a
Renato200419 <renato.olivera.c@uni.pe>
2024-09-08 11:37:07 -0500

    Se actualiza el archivo

--- Actividad2/devops-practice/tests/app.test.js
+++ Actividad2/devops-practice/tests/app.test.js
@@ -1,22 +1,20 @@
-// Importamos el módulo 'supertest' que se usa para hacer solicitudes HTTP en nuestras pruebas.
 const request = require('supertest');
-// Importamos la instancia de la aplicación y el servidor desde el archivo de la aplicación.
-const { app, server } = require('../src/app');  
+const app = require('../src/app');
 
-// Definimos un bloque de pruebas para la ruta GET '/'
 describe('GET /', () => {
-    // Después de ejecutar todas las pruebas en este bloque, cerramos el servidor para liberar el puerto.
-    afterAll(() => {
-        server.close();  // Cierra el servidor después de completar las pruebas
+    let server;
+
+    beforeAll((done) => {
+        server = app.listen(0, done); // Esto permite al sistema asignar un puerto libre automáticamente
     });
 
-    // Definimos una prueba que verifica la respuesta de la ruta '/'
+    afterAll((done) => {
+        server.close(done); // Cierra el servidor después de que las pruebas hayan terminado
+    });
+
     it('should return Hello, World!', async () => {
-        // Hacemos una solicitud GET a la ruta '/'
-        const res = await request(app).get('/');
-        // Verificamos que el código de estado de la respuesta sea 200 (OK)
+        const res = await request(server).get('/');
         expect(res.statusCode).toEqual(200);
-        // Verificamos que el texto de la respuesta sea 'Hello, World!'
         expect(res.text).toBe('Hello, World!');
     });
 });
